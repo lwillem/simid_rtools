5b2a38acd6c4719c50b6b8d25cbf7426a17e15fd
lwillem <lander.willem@uantwerpen.be>
2021-11-10 14:07:35 +0100

    Updated email notifications in the job_vaughan_param job script.

--- tutorial_vsc_vaughan/job_vaughan_param.sh
+++ tutorial_vsc_vaughan/job_vaughan_param.sh
@@ -6,14 +6,9 @@
 # - add your email adress to receive notifications
 # - copy the 'tutorial_vsc_vaughan' folder to ${VSC_DATA}
 
-# FYI on parallel setup
-# if you want one process that can use 16 cores for multithreading: --ntasks=1 --cpus-per-task=16
-# if you want to launch 16 independent processes (no communication): --ntasks=16
-# if you use mpi and do not care about where those cores are distributed: --ntasks=16
-
 # job settings:
 #SBATCH --output=vsc_tutorial_%j.txt
-#SBATCH --mail-user=lander.willem@uantwerpen.be
+#SBATCH --mail-user=xxx.xxx@uantwerpen.be
 #SBATCH --mail-type=END
 #SBATCH -t 00:05:00
 #SBATCH --ntasks=1 --cpus-per-task=1
